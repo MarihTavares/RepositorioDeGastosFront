9bf993335ac8cb5a294e2f7295e8b68cda0fbec9
MarihTavares <marina.ar1308@gmail.com>
2026-05-29 01:09:10 -0300

    lista de lançamentos

--- src/components/lancamentos.jsx
+++ src/components/lancamentos.jsx
@@ -12,18 +12,37 @@ function Lancamentos() {
     buscarLancamento()
   }, []);
 
+  const corSaldo = (tipo) => {
+    if (tipo === 'ENTRADA') return 'cards_entrada'
+    if (tipo === 'SAIDA') return 'cards_saida'
+  }
+
   return (
-    <div class="container py-4">
+    <div class="container p-2 mt-3" id="formulario">
 
-      <div id="formulario" class="col-12">
-        <div class="row"> <p>Lançamentos</p> </div>
-        <div class="row">
-          {lancamentos.map((l, i) => (
-            <p key={i}>{l.descricao} - R$ {l.valor} - {l.categoria} - {l.tipo}</p>
-          ))}
-        </div>
+      <div class="row px-3 titulo"> <p>Lançamentos</p> </div>
+      <div class="row px-3">
+        {lancamentos.map((l, i) => (
+          <div key={i} class="d-flex justify-content-between align-items-center px-3 lancamento-item">
+
+            <div>
+              <p class="m-0 fw-bold">{l.descricao}</p>
+              <p class="mb-1 subtitulo">{l.categoria}</p>
+            </div>
+
+            <div class="d-flex align-items-center gap-3">
+              <span class={`tag_tipo ${l.tipo === 'ENTRADA' ? 'tag_entrada' : 'tag_saida'}`}>
+                {l.tipo === 'ENTRADA' ? 'Entrada' : 'Saída'}
+              </span>
+              <span class={corSaldo(l.tipo)}>
+                R$ {l.valor} 
+              </span>
+            </div>
+
+          </div>
+        ))}
       </div>
-
+      
     </div>
   )
 }
