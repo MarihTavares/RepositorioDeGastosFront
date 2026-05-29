184fb02975c52f14ff4114b2df58c571538743a1
MarihTavares <marina.ar1308@gmail.com>
2026-05-29 02:02:45 -0300

    media estilizada

--- src/components/media.jsx
+++ src/components/media.jsx
@@ -12,18 +12,32 @@ function Media() {
     buscarMedia()
   }, []);
 
+  const maiorMedia = Math.max(...Object.values(medias))
+
   return (
-    <div class="container py-4">
+    <div class="container mt-3 mb-5 p-3" id='formulario'>
       
-      <div>
-        <div class="row"> <p>Média por categoria</p> </div>
-        <div class="row">
-          {Object.entries(medias).map(([cat, media]) => (
-            <p key={cat}>{cat}: R$ {media.toFixed(2)}</p>
-          ))}
-        </div>
+      <div class="row px-3 titulo"> 
+        <p>Média por categoria</p> 
       </div>
 
+      <div class="row">
+        {Object.entries(medias).map(([cat, media]) => (
+          <div className="d-flex align-items-center gap-3 px-3 lancamento-item" key={cat}>
+            <span class="px-3 fs-6 fw-bold categoria-nome">{cat}</span>
+
+            <div className="barra-container">
+              <div 
+                className="barra-progresso"
+                style={{ width: `${(media / maiorMedia) * 100}%` }}
+              />
+            </div>
+
+            <span className="categoria-valor px-1">R$ {media.toFixed(0)}</span>
+          </div>
+        ))}
+      </div>
+      
     </div>
   )
 }
